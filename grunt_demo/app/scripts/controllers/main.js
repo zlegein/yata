7fdcdd33d9142218ccd21b3c6aeb8a554c648ee5
Zach Legein <zlegein@gmail.com>
2015-08-08 18:19:10 -0500

    initial commit

---
+++ grunt_demo/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name yataApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the yataApp
+ */
+angular.module('yataApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
